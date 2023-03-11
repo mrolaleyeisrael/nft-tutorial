24fb0d54b55d135d44b1580fafa8ac9b1d8c06ab
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-03-12 00:50:50 +0100

    Minted 0xdaC6C12901E67AB57708C8b39553b92bad2bD003

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 // Import the openzepplin contracts
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-require "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // NFTee is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
-contract NFTee is ERC721{
-    constructor() ERC721("NFTee", "NFT") {
+contract NFTee is ERC721 {
+    constructor() ERC721("Independence NFT", "I-NFT") {
         _mint(msg.sender, 1);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: "env" });
+require("dotenv").config();
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: "0.8.18",
 	networks: {
-		goerli: {
-			url: QUICKNODE_HTTP_URL,
-			accounts: PRIVATE_KEY,
+		sepolia: {
+			url: process.env.QUICKNODE_HTTP_URL,
+			accounts: [process.env.PRIVATE_KEY],
 		},
 	},
 };
